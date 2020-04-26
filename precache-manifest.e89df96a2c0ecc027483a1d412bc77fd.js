6bb7dca344f18b7990a179f53853502abff2aab5
Paulina Dawidziuk <paulina-dawidziuk@wp.pl>
2020-04-26 17:25:06 +0200

    Updates

---
+++ precache-manifest.e89df96a2c0ecc027483a1d412bc77fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5696c6e95b088f78d1fe4d0018288f4",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "b296bfd772ebc985379d",
+    "url": "/react-gh-pages/static/css/main.838dc9dd.chunk.css"
+  },
+  {
+    "revision": "c1ad307273e71af93bd4",
+    "url": "/react-gh-pages/static/js/2.5b6c759c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.5b6c759c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b296bfd772ebc985379d",
+    "url": "/react-gh-pages/static/js/main.bd7ed14c.chunk.js"
+  },
+  {
+    "revision": "48a5ca2a5baccf2e02a1",
+    "url": "/react-gh-pages/static/js/runtime-main.379b9c3c.js"
+  }
+]);
